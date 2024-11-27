f403dd5657e529c346bf8cec9770e9328253bd92
Arvindh M <arvindh.m@capgemini.com>
2024-11-27 14:17:48 +0530

    Azure test webapp3 push

---
+++ WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public IActionResult Message()
+        {
+            return Ok("Jungle sometimes");
+        }
+    }
+}
